aef732ff79f5e1c24d9ea0b8a433d3023909bf0d
Noelao Noe <113521714+noelao@users.noreply.github.com>
2025-05-17 17:39:54 +0700

    Add files via upload

--- trying1/scroll.js
+++ trying1/scroll.js
@@ -12,3 +12,12 @@ window.addEventListener('scroll', function() {
         headerIni.style.boxShadow = "rgba(255, 255, 255, 0.06) 0px 6px 24px";
     }
 });
+
+const navBila = document.querySelector(".bawah .navKiri .asli")
+let tinggiWeb = window.innerHeight
+console.log(navBila.clientHeight)
+if(navBila.clientHeight >= tinggiWeb - 100){
+    navBila.style.height = "calc(100vh - 100px)";
+    navBila.style.overflowY = "scroll";
+}
+
